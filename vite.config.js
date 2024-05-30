3c64cc3825b1e7f669379134e46ca2d7e55c7f9b
josias eliel marrufo chable <josias@pinchaaqui.es>
2024-05-30 17:46:28 -0600

    add line build reference

--- vite.config.js
+++ vite.config.js
@@ -2,11 +2,13 @@ import { fileURLToPath, URL } from 'node:url'
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import laravel from 'laravel-vite-plugin';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
+    laravel(['src/main.js']),
   ],
   css: {
     preprocessorOptions: {
